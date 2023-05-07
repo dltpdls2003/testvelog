5e0f916b9dc6d0c01976ceab36bc68b3e31724bb
dltpdls2003 <dltpdls2003@naver.com>
2023-05-08 06:51:27 +0900

    Login

--- front/src/components/Header/Header.tsx
+++ front/src/components/Header/Header.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useRef, useState } from "react"
+ import React, { useEffect, useRef, useState } from "react"
 import { Outlet } from "react-router-dom"
 import styled from "styled-components"
 import Logo from "./logo"
